9d68c1a1f721f133d95744b5c0ac3825468ee21f
anuja05vaishnava <60468761+anuja05vaishnava@users.noreply.github.com>
2020-02-26 19:31:08 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,52 @@
+
+
+
+var s = "alvin".substr(5, 0);
+var s1 = "alice".slice(1, 5);
+console.log(s);
+console.log(s1);
+
+
+var nums = [ 1, 1, 2, 3, 5, 8 ];
+var s = nums.join();
+console.log(nums);
+console.log(s);
+
+var input = prompt('What is your Name?');
+var inputArray = [];
+var size = 5; 
+
+
+for(var i=0; i<size; i++) {
+	
+	Taking Input from user
+	inputArray[i] = prompt('Enter Element ' + (i+1));
+}
+
+
+
+
+function myData() { 
+   return 123; 
+} 
+console.log(myData()); 
+
+function myValue() { 
+} 
+console.log(myValue()); 
+
+
+
+
+
+var user = {
+     first_name: "Anuja",
+     last_name: "vaishnava",
+     age: 4
+     website: "anuja.vaishnava"
+};
+
+for (key in user) {
+     console.log(key);
+}
+The code ab
