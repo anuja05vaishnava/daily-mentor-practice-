febbfa0cdae0645a8b76299b722c1ba9166c3938
anuja05vaishnava <60468761+anuja05vaishnava@users.noreply.github.com>
2020-03-02 18:29:44 +0530

    Add files via upload

---
+++ childprocess.js
@@ -0,0 +1,56 @@
+const { spawn } = require('child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+const { exec, spawn } = require('child_process');
+exec('my.bat', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+
+const bat = spawn('"my script.cmd"', ['a', 'b'], { shell: true });
+
+exec('"my script.cmd" a b', (err, stdout, stderr) => {
+  
+});
+
+
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+
+  
+  http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+
+  console.log(`Worker ${process.pid} started`);
+}
